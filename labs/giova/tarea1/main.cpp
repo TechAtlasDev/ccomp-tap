bbfb48934b42d1adf36b637a7b0b0f41e3fa353f
Giovanny A. Jimenez <156117774+TechAtlasDev@users.noreply.github.com>
2026-05-21 15:05:34 -0500

    Refactor merge sort with static and dynamic comparators

--- labs/giova/tarea1/main.cpp
+++ labs/giova/tarea1/main.cpp
@@ -5,115 +5,110 @@
 using namespace std;
 
 template <class C>
-struct Less {
-
-    bool operator()(C a, C b) {
-        return a < b;
-    }
-};
-
-template <class C>
-struct Greater {
-
-    bool operator()(C a, C b) {
-        return a > b;
-    }
+struct LessEstatico {
+    inline bool operator()(const C& a, const C& b) const { return a < b; }
 };
 
 template <class C, class Comparator>
-void merge(C* ar,
-           int left,
-           int mid,
-           int right,
-           Comparator cmp) {
-
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    C* L = new C[n1];
-    C* R = new C[n2];
-
-    for(int i = 0; i < n1; i++)
-        L[i] = ar[left + i];
-
-    for(int j = 0; j < n2; j++)
-        R[j] = ar[mid + 1 + j];
-
-    int i = 0;
-    int j = 0;
-    int k = left;
-
-    while(i < n1 && j < n2) {
-
-        // TEMPLATE
-        if(cmp(L[i], R[j])) {
-            ar[k++] = L[i++];
-        }
-        else {
-            ar[k++] = R[j++];
-        }
+void mergeEstatico(C* ar, C* temp, int left, int mid, int right, Comparator cmp) {
+    int i = left, j = mid + 1, k = left;
+    for (int idx = left; idx <= right; idx++) temp[idx] = ar[idx];
+    while (i <= mid && j <= right) {
+        if (cmp(temp[i], temp[j])) ar[k++] = temp[i++];
+        else ar[k++] = temp[j++];
     }
-
-    while(i < n1)
-        ar[k++] = L[i++];
-
-    while(j < n2)
-        ar[k++] = R[j++];
-
-    delete[] L;
-    delete[] R;
+    while (i <= mid) ar[k++] = temp[i++];
 }
 
 template <class C, class Comparator>
-void mergeSort(C* ar,
-               int left,
-               int right,
-               Comparator cmp) {
+void mergeSortEstaticoInternal(C* ar, C* temp, int left, int right, Comparator cmp) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSortEstaticoInternal(ar, temp, left, mid, cmp);
+        mergeSortEstaticoInternal(ar, temp, mid + 1, right, cmp);
+        mergeEstatico(ar, temp, left, mid, right, cmp);
+    }
+}
 
-    if(left < right) {
+template <class C, class Comparator>
+void mergeSortEstatico(C* ar, int n, Comparator cmp) {
+    C* temp = new C[n];
+    mergeSortEstaticoInternal(ar, temp, 0, n - 1, cmp);
+    delete[] temp;
+}
 
-        int mid = (left + right) / 2;
+template <class C>
+struct ComparatorDinamico {
+    virtual bool compare(const C& a, const C& b) const = 0;
+    virtual ~ComparatorDinamico() {}
+};
 
-        mergeSort(ar, left, mid, cmp);
-        mergeSort(ar, mid + 1, right, cmp);
+template <class C>
+struct LessDinamico : public ComparatorDinamico<C> {
+    bool compare(const C& a, const C& b) const override { return a < b; }
+};
 
-        merge(ar, left, mid, right, cmp);
+template <class C>
+void mergeDinamico(C* ar, C* temp, int left, int mid, int right, const ComparatorDinamico<C>& cmp) {
+    int i = left, j = mid + 1, k = left;
+    for (int idx = left; idx <= right; idx++) temp[idx] = ar[idx];
+    while (i <= mid && j <= right) {
+        if (cmp.compare(temp[i], temp[j])) ar[k++] = temp[i++];
+        else ar[k++] = temp[j++];
+    }
+    while (i <= mid) ar[k++] = temp[i++];
+}
+
+template <class C>
+void mergeSortDinamicoInternal(C* ar, C* temp, int left, int right, const ComparatorDinamico<C>& cmp) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSortDinamicoInternal(ar, temp, left, mid, cmp);
+        mergeSortDinamicoInternal(ar, temp, mid + 1, right, cmp);
+        mergeDinamico(ar, temp, left, mid, right, cmp);
     }
 }
 
 template <class C>
-void print(C* ar, int n) {
-
-    for(int i = 0; i < n; i++)
-        cout << ar[i] << " ";
-
-    cout << endl;
+void mergeSortDinamico(C* ar, int n, const ComparatorDinamico<C>& cmp) {
+    C* temp = new C[n];
+    mergeSortDinamicoInternal(ar, temp, 0, n - 1, cmp);
+    delete[] temp;
 }
 
 int main() {
+    int cantidad_filas = 100000000;
 
-    const int N = 50000;
+    cout << "Estatico,Dinamico\n";
 
-    int* ar = new int[N];
+    for (int idx = 1; idx <= cantidad_filas; idx++) {
+        int N = idx * 25000;
+        int* ar1 = new int[N];
+        int* ar2 = new int[N];
 
-    for(int i = 0; i < N; i++)
-        ar[i] = rand();
+        for (int i = 0; i < N; i++) {
+            int num = rand();
+            ar1[i] = num;
+            ar2[i] = num;
+        }
 
-    Less<int> cmp;
+        LessEstatico<int> cmpEstatico;
+        auto inicio = chrono::high_resolution_clock::now();
+        mergeSortEstatico(ar1, N, cmpEstatico);
+        auto fin = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> tEstatico = fin - inicio;
 
-    auto inicio = chrono::high_resolution_clock::now();
+        LessDinamico<int> cmpDinamico;
+        inicio = chrono::high_resolution_clock::now();
+        mergeSortDinamico(ar2, N, cmpDinamico);
+        fin = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> tDinamico = fin - inicio;
 
-    mergeSort(ar, 0, N - 1, cmp);
+        cout << tEstatico.count() << "," << tDinamico.count() << "\n";
 
-    auto fin = chrono::high_resolution_clock::now();
-
-    chrono::duration<double, milli> tiempo = fin - inicio;
-
-    cout << "Tiempo templates: "
-         << tiempo.count()
-         << " ms" << endl;
-
-    delete[] ar;
+        delete[] ar1;
+        delete[] ar2;
+    }
 
     return 0;
 }
